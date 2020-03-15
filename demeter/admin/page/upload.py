8e6d2eb04923661ab5d7a84128130553eb05760f
dever <dm@dever.cc>
2020-03-15 12:15:52 +0000

    edit

--- demeter/admin/page/upload.py
+++ demeter/admin/page/upload.py
@@ -20,10 +20,10 @@ class upload_path(Load):
 		day = day.split('-')
 		for meta in file_metas:
 			name = meta['filename']
-			print(name)
+			temp = name.split('.')
 			file_name =  str(uuid.uuid5(uuid.uuid1(), 'file'))
 			file_path = day[0] + '/' + day[1] + '/' + day[2]
-			file_path = File.mkdirs(os.path.join(Demeter.path, 'runtime','upload', file_path)) + '/' + Demeter.md5(file_name) + '.jpg'
+			file_path = File.mkdirs(os.path.join(Demeter.path, 'runtime','upload', file_path)) + '/' + Demeter.md5(file_name) + '.' + temp[1]
 			with open(file_path, 'wb') as up:
 				up.write(meta['body'])
 		self.out('yes', {'src':url + file_path.replace(Demeter.path + 'runtime', '')})
